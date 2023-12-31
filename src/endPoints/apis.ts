adc24895a0f66518506f56697c946c3261560b1d
Pouya <p.mohammadkhani95@gmail.com>
2023-12-31 23:02:11 +0330

    remove apis for now

--- src/endPoints/apis.ts
+++
@@ -1,8 +0,0 @@
-import { useFetch } from '@/composables/useFetch';
-
-export const apis = {
-  getCoinsList(vs_currency: string = 'usd') {
-    // Name, Symbol, Current_price
-    return useFetch(`coins/markets?vs_currency=${vs_currency}`);
-  },
-};
